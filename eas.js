bc48359e332a440b1032ad035559bfbd0c39e578
2pavel <pawelnwz@gmail.com>
2020-05-15 18:11:41 +0200

    added square 16 x 16 pixels board with black mouseover

--- eas.js
+++ eas.js
@@ -1,16 +1,42 @@
 const painting = document.querySelector('#painting');
-const creatediv = document.createElement('div');
 let rows = 16;
-let divArray = [];
+//let divArray = [];
+let row = 1;
+let column = 1;
+
+function appendChildren (parent, children) {
+    children.forEach(function (child) {
+        parent.appendChild(child);
+    });
+}
 
 function fillPainting() {
 
-    for(i = 0; i <= rows * rows; i++) {
-        
-        creatediv.setAttribute('class', 'pixel');
-        divArray.push(creatediv);
+    for(let i = 0; i < rows * rows; i++) {
+        let creatediv = document.createElement('div');
+        creatediv.addEventListener('mouseover', () => {
+            creatediv.style.backgroundColor = 'black';
+        });
+        creatediv.classList.add(`pixel`);
+        creatediv.style.backgroundColor = 'white';
+        creatediv.style.gridRow = row;
+        //creatediv.style.gridRowEnd = row + 1;
+        creatediv.style.gridColumn = column;
+        //creatediv.style.gridColumnEnd = column + 1;
+        painting.appendChild(creatediv.cloneNode(true));
+        column += 1;
+        if (column == rows + 1) {
+            row += 1;
+            column = 1;
+        }
         
     }
 }
-
 fillPainting();
+const pixels = document.querySelectorAll('.pixel');
+    pixels.forEach((div) => {
+        div.addEventListener('mouseover', (e) => {
+        div.style.backgroundColor = 'black';
+        });
+    });
+
