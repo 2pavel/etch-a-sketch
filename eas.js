676ef9ce4ff30a60c137dd0ca9870872808941df
2pavel <pawelnwz@gmail.com>
2020-05-16 17:21:44 +0200

    almost finished, need to remove event listener for rainbow aand add pencil

--- eas.js
+++ eas.js
@@ -1,17 +1,35 @@
 const painting = document.querySelector('#painting');
 let rows = 64;
-//let divArray = [];
 let row = 1;
 let column = 1;
 
-function appendChildren (parent, children) {
-    children.forEach(function (child) {
-        parent.appendChild(child);
+function changeColor(e) {
+    usercolor = e.target.id;
+    removeEventListener('mouseover', () => {
+        div.style.backgroundColor = randomColor();
+    });
+}
+function randomColor(){
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+function rainbow() {
+    pixels.forEach((div) => {
+        div.addEventListener('mouseover', () => {
+            div.style.backgroundColor = randomColor();
+        });
     });
 }
 
 function clear(){
-
+    let erase = document.querySelectorAll('.pixel');
+    erase.forEach((e) => {
+        e.style.backgroundColor = 'white';
+    });
 }
 
 function fillPainting() {
@@ -34,11 +52,31 @@ function fillPainting() {
         
     }
 }
+let usercolor = 'black';
+let pencilbtn = document.getElementById('pencil');
+let randombtn = document.getElementById('randomclr');
+let colorbtns = document.querySelectorAll('.colorbtn');
+let rainbowbtn = document.getElementById('rainbow');
+let allbuttons = document.querySelectorAll('button');
+
 fillPainting();
+
 const pixels = document.querySelectorAll('.pixel');
     pixels.forEach((div) => {
         div.addEventListener('mouseover', (e) => {
-        div.style.backgroundColor = 'black';
+        div.style.backgroundColor = usercolor;
         });
     });
 
+colorbtns.forEach((e) => {
+    e.addEventListener('click', changeColor);
+});
+randombtn.addEventListener('click', () => {
+    usercolor = randomColor();
+});
+
+rainbowbtn.addEventListener('click', rainbow);
+
+
+let clearbtn = document.getElementById('clearbtn');
+clearbtn.addEventListener('click', clear);
